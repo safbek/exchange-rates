20120cf682e83bb2a134bf43128954af64605258
safbek <safbek@list.ru>
2022-03-27 14:17:41 +0300

    create app exchange-rates

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,76 @@
-import React from 'react';
-import logo from './logo.svg';
+import { Table, OverlayTrigger, Tooltip, Button } from 'react-bootstrap';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { useEffect, useState } from 'react';
+import { Currency } from '../src/types/currencies';
 
 function App() {
+
+  const [currencies, setCurrencies] = useState<Currency[] | null>(null);
+
+  useEffect(() => {
+      fetch("https://www.cbr-xml-daily.ru/daily_json.js")
+          .then((res) => res.json())
+          .then((json) => {
+            const currencyData: Currency[] = Object.values(json.Valute);
+            setCurrencies(currencyData);
+          })
+  },[]);
+
+  const rounded = function(number: number) {
+    return Math.round(number * 1e2) / 1e2;
+  }
+
+  const today = new Date().toLocaleDateString('ru', { year: 'numeric', month: '2-digit', day: '2-digit' });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <h1 className="text-center mt-3 mb-5">{`Курсы валют ЦБ РФ на ${today}`}</h1>
+
+        <Table striped bordered hover size="sm">
+          <thead>
+            <tr>
+              <th>Валюта</th>
+              <th>Курс</th>
+              <th>Разница с предыдущим днём в %</th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              currencies?.map(({CharCode, Value, Name, Previous}) => {
+                const currencyDiff = 100.0 * (Previous - Value) / Value;
+
+                return(
+                  <tr key={Name}>
+                    <td>
+                      {
+                        <OverlayTrigger
+                          key={Name}
+                          placement="bottom"
+                          overlay={
+                            <Tooltip id={`tooltip-${CharCode}`}>
+                              <strong>{Name}</strong>
+                            </Tooltip>
+                          }
+                        >
+                          <Button variant="transparent">{CharCode}</Button>
+                        </OverlayTrigger>
+                      }
+                    </td>
+                    <td>{Value}</td>
+                    <td className={currencyDiff > 0 ? 'text-success' : 'text-danger'}>
+                      {
+                        currencyDiff > 0 ? `↑ +${rounded(currencyDiff)}%` : `↓ ${Math.abs(rounded(currencyDiff))}%`
+                      }
+                    </td>
+                  </tr>
+                )
+              })
+            }
+          </tbody>
+        </Table>
+      </div>
     </div>
   );
 }
---
+++ src/types/currencies.ts
@@ -0,0 +1,9 @@
+export type Currency = {
+    CharCode: string,
+    ID: number,
+    Name: string,
+    Nominal: number,
+    NumCode: string,
+    Previous: number,
+    Value: number,
+}
